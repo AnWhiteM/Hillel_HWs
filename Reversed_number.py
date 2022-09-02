5424df65a070f136181241b59aa62d363cac8588
AnWhiteM <94605350+AnWhiteM@users.noreply.github.com>
2022-09-02 14:45:56 +0300

    Add files via upload

---
+++ Reversed_number.py
@@ -0,0 +1,23 @@
+user_number = int(input("Please Input 5-digit number: "))
+rev = 0
+num = user_number % 10
+user_number = user_number // 10
+rev = rev * 10
+rev = rev + num
+num = user_number % 10
+user_number = user_number // 10
+rev = rev * 10
+rev = rev + num
+num = user_number % 10
+user_number = user_number // 10
+rev = rev * 10
+rev = rev + num
+num = user_number % 10
+user_number = user_number // 10
+rev = rev * 10
+rev = rev + num
+num = user_number % 10
+user_number = user_number // 10
+rev = rev * 10
+rev = rev + num
+print(str(rev))
