78ed9bfdffb0ea3c1146468c660d7a3d325edd32
AnWhiteM <94605350+AnWhiteM@users.noreply.github.com>
2022-09-09 13:56:01 +0300

    Update dividing_list.py

--- dividing_list.py
+++ dividing_list.py
@@ -1,6 +1,10 @@
 new_lst = [1, 2, 3, 4, 5, 6]
-first_lst = new_lst[:3]
-second_lst = new_lst[3:]
+l = int(len(new_lst) / -2)
+print(l)
+
+first_lst = new_lst[:l]
+second_lst = new_lst[l:]
+
 new_lst = []
 new_lst = [first_lst, second_lst]
 print(str(new_lst))
