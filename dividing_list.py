ab21251689fca12a65fb08e069a8a7da9e09b912
AnWhiteM <94605350+AnWhiteM@users.noreply.github.com>
2022-09-09 19:47:51 +0300

    Update dividing_list.py

--- dividing_list.py
+++ dividing_list.py
@@ -1,10 +1,31 @@
-new_lst = [1, 2, 3, 4, 5, 6]
-l = int(len(new_lst) / -2)
-print(l)
+# [1, 2, 3, 4, 5, 6] => [[1, 2, 3], [4, 5, 6]]
+# [1, 2, 3] => [[1, 2], [3]]
+# [1, 2, 3, 4, 5] => [[1, 2, 3], [4, 5]]
+# [1] => [[1], []]
+# [] => [[], []]
 
-first_lst = new_lst[:l]
-second_lst = new_lst[l:]
+new_lst = [1, 2, 3, 4, 5, 6, 5, 7, 9, 10, 11]
+l = len(new_lst)
+if l % 2 == 0:
+    hl = l // 2
+    first_lst = new_lst[:hl]
+    second_lst = new_lst[hl:]
+    new_lst = []
+    new_lst0 = [first_lst, second_lst]
+    print(str(new_lst0))
+elif l == 1:
+    hl = int(l % 2)
+    first_lst = new_lst[:hl]
+    second_lst = new_lst[hl:]
+    new_lst = []
+    new_lst2 = [first_lst, second_lst]
+    print(str(new_lst2))
+elif l % 2 != 0:
+    hl = int(l / -2)
+    first_lst = new_lst[:hl]
+    second_lst = new_lst[hl:]
+    new_lst = []
+    new_lst1 = [first_lst, second_lst]
+    print(str(new_lst1))
 
-new_lst = []
-new_lst = [first_lst, second_lst]
-print(str(new_lst))
+# Только так получилось :'(
