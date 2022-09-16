a5fc498036b2c5f3d83f910137465149dc00d58d
AnWhiteM <94605350+AnWhiteM@users.noreply.github.com>
2022-09-16 14:48:26 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,27 @@
-op = input("Please enter operation(+,- etc.): ")
-n1 = float(input("Please enter 1st number: "))
-n2 = float(input("Please enter 2nd number: "))
+while True:
+    op = input("Please enter operation(+,- etc.): ")
+    n1 = float(input("Please enter 1st number: "))
+    n2 = float(input("Please enter 2nd number: "))
 
-if op == '+':
-    r = n1 + n2
-    print("Result: " + str(r))
-elif op == '-':
-    r = n1 - n2
-    print("Result: " + str(r))
-elif op == '*':
-    r = n1 * n2
-    print("Result: " + str(r))
-elif op == '/':
-    if n2 == 0:
-        print("Division by zero is not allowed")
+    if op == '+':
+            r = n1 + n2
+            print("Result: " + str(r))
+    elif op == '-':
+            r = n1 - n2
+            print("Result: " + str(r))
+    elif op == '*':
+            r = n1 * n2
+            print("Result: " + str(r))
+    elif op == '/':
+        if n2 == 0:
+                print("Division by zero is not allowed")
+        else:
+                r = n1 / n2
+                print("Result: " + str(r))
+    question = input("Do you want to continue (y for yes/n for no) : ")
+    question = question.lower()
+    if question == "n":
+        break
     else:
-        r = n1 / n2
-        print("Result: " + str(r))
+        continue
+
