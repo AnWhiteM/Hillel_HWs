36cf8adfd1b1af267986c4b0c4bba1748ffbf511
AnWhiteM <ivangrivtsov0@gmail.com>
2022-10-29 09:49:29 +0300

    module

---
+++ Student_group/st_group.py
@@ -0,0 +1,24 @@
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        res = self.find_student(last_name)
+        self.group.discard(res)
+
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = ''
+        for student in self.group:
+            all_students += f'{str(student)}\n'
+        return f'Number:{self.number}\n{all_students} '
